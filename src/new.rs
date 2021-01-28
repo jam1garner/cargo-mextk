4acbd815745aa07536a0bd24e9b2b6efe80326b9
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-01-28 00:18:13 -0500

    Fix new project creation

--- src/new.rs
+++ src/new.rs
@@ -27,10 +27,21 @@ fn extract_recursive(to: &Path, dir: &Dir) -> io::Result<()> {
     }
 
     for file in dir.files() {
-        fs::write(to.join(file.path()), file.contents())?;
+        match file.path() {
+            path if path.to_string_lossy() == "Cargo.toml" => {
+                fs::write(
+                    to.join("Cargo.toml"),
+                    file.contents_utf8()
+                        .unwrap()
+                        .replace("{{template_name}}", to.to_string_lossy().as_ref())
+                )?
+            }
+            relative_path => fs::write(to.join(relative_path), file.contents())?
+        }
     }
 
     for child_dir in dir.dirs() {
+        fs::create_dir_all(to.join(child_dir.path()))?;
         extract_recursive(to, child_dir)?;
     }
 
